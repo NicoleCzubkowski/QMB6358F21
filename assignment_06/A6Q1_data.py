be3386fd0d91e5ca51620028d7de0eeeb31da71a
NicoleCzubkowski <89553108+NicoleCzubkowski@users.noreply.github.com>
2021-10-22 14:07:07 -0400

    It works!
    Question 1 done

--- assignment_06/A6Q1_data.py
+++ assignment_06/A6Q1_data.py
@@ -6,11 +6,11 @@
 #
 # Sample script for Assignment 6, Question 1
 #
-# Name: 
+# Name: Nicole Czubkowski 
 # College of Business Administration
 # University of Central Florida
 #
-# Date:
+# Date: 10/20/21
 #
 #
 ##################################################
@@ -28,9 +28,6 @@ import pandas as pd # To read and inspect data
 import statsmodels.formula.api as sm # Another way to estimate linear regression
 
 
-# Optional:
-import glob
-
 
 ##################################################
 # Set Working Directory.
@@ -40,48 +37,60 @@ import glob
 # Find out the current directory.
 os.getcwd()
 # Change to a new directory.
-os.chdir('C:\\Users\\le279259\\Documents\\Teaching\\QMB6358F21\\assignment_06')
+os.chdir('C:\\Users\\19548\\Documents\\WEDNS-CLASS1\\assignment_06')
 # Check that the change was successful.
 os.getcwd()
 
+####################
+# List file names 
+####################
+path = 'C:\\Users\\19548\\Documents\\QMB6358F21\\QMB6358F21\\assignment_06\\housing_data'
 
-##################################################
+House_data = os.listdir(path)
+
+os.chdir('C:\\Users\\19548\\Documents\\QMB6358F21\\QMB6358F21\\assignment_06\\housing_data')
+
+housing_single = []
+housing_full = []
+for i in House_data:
+        #list file names
+        print(i)
+        
+ ##################################################
 # Load Data.
 ##################################################
 
 # Loading one file at a time:
-housing_full = pd.read_csv('housing_data/housing_data_1.csv')
-
-
-
+       
+        #reading the files
+        housing_single = pd.read_csv(i, index_col=None)
 # Use a for loop to bind additional datasets to housing_full.
 # Code goes here.
 
-
-
+        #combining the files 
+        housing_full = pd.concat([pd.read_csv(i) for i in House_data])
+        housing_full.head()
+    
+##################################################
+# Load Data.
+##################################################
+# Loading one file at a time:
+# Use a for loop to bind additional datasets to housing_full.
+# Code goes here.
 # Calculate summary statistics for your data.
-housing_full.describe()
+print(housing_full.describe())
 # Use this to check whether your data handling is working correctly.
-
 
 ##################################################
 # Fit the Regression Model
 ##################################################
 
 # After the full dataset is obtained:
-
 # Fit the regression model.
 reg_model_full_sm = sm.ols(formula = "house_price ~ income + in_cali + earthquake", 
                            data = housing_full).fit()
-
-
-
 # Display a summary table of regression results.
 print(reg_model_full_sm.summary())
-
-
-
-
 
 ##################################################
 # End
