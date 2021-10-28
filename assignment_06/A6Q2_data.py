298a8511a64ef846047718eb8bceeee23301aa80
NicoleCzubkowski <89553108+NicoleCzubkowski@users.noreply.github.com>
2021-10-27 23:02:55 -0400

    Part A and B
    Works!

--- assignment_06/A6Q2_data.py
+++ assignment_06/A6Q2_data.py
@@ -6,11 +6,11 @@
 #
 # Sample script for Assignment 6, Question 2
 #
-# Name: 
+# Name: Nicole Czubkowski 
 # College of Business Administration
 # University of Central Florida
 #
-# Date:
+# Date:10/25/2021
 #
 #
 ##################################################
@@ -26,7 +26,8 @@ import os # To set working directory
 # import numpy as np # Not needed here but often useful
 import pandas as pd # To read and inspect data
 import statsmodels.formula.api as sm # Another way to estimate linear regression
-
+import re
+import itertools
 
 
 ##################################################
@@ -35,35 +36,68 @@ import statsmodels.formula.api as sm # Another way to estimate linear regression
 
 
 # Find out the current directory.
+#os.getcwd()
+# Change to a new directory.
+#os.chdir('C:\\Users\\le279259\\Documents\\Teaching\\QMB6358F21\\assignment_06')
+# Check that the change was successful.
+#os.getcwd()
+
+# Find out the current directory.
 os.getcwd()
 # Change to a new directory.
-os.chdir('C:\\Users\\le279259\\Documents\\Teaching\\QMB6358F21\\assignment_06')
+os.chdir('C:\\Users\\19548\\Documents\\WEDNS-CLASS1\\assignment_06\\housing_tables')
 # Check that the change was successful.
 os.getcwd()
 
 
-
-##################################################
-# Get all observations from all 5 files
-# by creating functions to read each file
-# then read the data and collect in a 
-# data frame called housing_full.
-##################################################
+##rule to determine if a line is data and to pull the numbers out or not 
+##Reading files
+path = 'C:\\Users\\19548\\Documents\\WEDNS-CLASS1\\assignment_06\\housing_tables'
+House_records = os.listdir(path)
 
 
+# def read_file(is_data_row):
+#     with open(is_data_row,'r') as f :
+#         print(f.read())
+        
+# is_data_row = []
+# for i in House_records:
+#     if i.endswith(".tex"):
+#         with open(i,'r') as read:
+        
+#            for line in read:
+#                #get_numbers = is_data_row(line)
+#                 if line[0].isdigit():
+#                     is_data_row.append(line[0:])
+        
+#print(is_data_row)
 
+#########
+#Part A
+#########
+def is_data_row():
+    with open ("housing_sales_week_1_day_1.tex", 'r') as f:
+        for line in f :
+            if line[0].isdigit():
+                
+                print(line.strip())
+is_data_row()
 
+##########
+#Part B 
+# #####
 
-# Replace this with the final dataset.
-housing_full = pd.read_csv('housing_data/housing_data_1.csv')
-# For now, this is just an example from Question 1
-# to see what the result should look like. 
+def get_obs_row():
+    with open ("housing_sales_week_1_day_1.tex", 'r') as f:
+        for line in itertools.islice(f,9,28):
+            #if line[0].isdigit():
+               data = [float(s) for s in re.findall('\d*\.?\d+',line)]
+               print(data)
+get_obs_row()   
 
-
-
-
-
-
+#########
+#Part C
+##########         
 ##################################################
 # Inspect the final dataset
 ##################################################
